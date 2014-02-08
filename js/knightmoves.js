e36ee3b76a8fd63c3b3b5543e50b1be3161ecb18
Michael Delfino <megabeano@gmail.com>
2014-02-07 23:57:12 -0500

    Only only legal knight moves

--- js/knightmoves.js
+++ js/knightmoves.js
@@ -1,8 +1,10 @@
 var board,
     boardElem = $('#board'),
     start = 'a1',
+    cur = 'a1',
     dest = 'h8',
-    numMoves = 0;
+    numMoves = 0,
+    minMoves = 1;
 
 function randomInt(min, max) {
     return Math.floor((Math.random()*(max-min+1))+min);
@@ -16,6 +18,7 @@ function restart() {
     numMoves = 0;
     $("#num_moves").text(numMoves);
     start = randomSquare();
+    cur = start;
     dest = randomSquare();
     while (start == dest) {
         dest = randomSquare();
@@ -27,9 +30,44 @@ function restart() {
     boardElem.find('.square-' + dest).addClass('highlight');
 }
 
-function onDrop() {
+function generate_moves(square) {
+    function rowColToSquare(r, c) {
+        if (r < 0 || r > 7 || c < 0 || c > 7) return 'invalid';
+        return 'abcdefgh'[c] + (r+1);
+    }
+    function pushIfValid(list, item) {
+        if (item != 'invalid') list.push(item);
+        return list;
+    }
+    var row = parseInt(square[1]) - 1,
+        col = 'abcdefgh'.indexOf(square[0]),
+        moves = [];
+    pushIfValid(moves, rowColToSquare(row-2,col-1));
+    pushIfValid(moves, rowColToSquare(row-1,col-2));
+    pushIfValid(moves, rowColToSquare(row+2,col-1));
+    pushIfValid(moves, rowColToSquare(row+1,col-2));
+    pushIfValid(moves, rowColToSquare(row+2,col+1));
+    pushIfValid(moves, rowColToSquare(row+1,col+2));
+    pushIfValid(moves, rowColToSquare(row-2,col+1));
+    pushIfValid(moves, rowColToSquare(row-1,col+2));
+    return moves;
+}
+
+function onDrop(source, target) {
+    if (generate_moves(source).indexOf(target) == -1) return 'snapback';
     numMoves++;
     $("#num_moves").text(numMoves);
+    var pos = {};
+    if (target == dest) {
+        setTimeout(function() {
+            //if (numMoves == minMoves) {
+                alert('Great job!');
+            //} else {
+                //alert('Good job, but you could have gotten there in fewer moves.');
+            //}
+            restart();
+        }, 100);
+    }
 }
 
 var init = function() {
