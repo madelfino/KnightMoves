16b40ef213d4260bde1395f9ced955c05da5574e
Michael Delfino <megabeano@gmail.com>
2014-02-07 21:57:09 -0500

    Generate position with knight placed randomly

--- js/knightmoves.js
+++ js/knightmoves.js
@@ -1,13 +1,36 @@
+var board,
+    start,
+    dest;
+
+function randomInt(min, max) {
+    return Math.floor((Math.random()*(max-min+1))+min);
+}
+
+function randomSquare() {
+    return 'abcdefgh'[randomInt(0,7)] + randomInt(1,8);
+}
+
+function restart() {
+    start = randomSquare();
+    dest = randomSquare();
+    while (start == dest) {
+        dest = randomSquare();
+    }
+    var pos = {};
+    pos[start] = 'wN'
+    board.position(pos, false);
+}
+
 var init = function() {
+    board = new ChessBoard('board', {
+        draggable: true,
+        showNotation: false
+    });
+    restart();
 
-var board;
-
-var cfg = {
-    draggable: true,
-    showNotation: false
-};
-
-board = new ChessBoard('board', cfg)
+    $('#start').click(function() {
+        restart();
+    });
 
 }; //end init
 
